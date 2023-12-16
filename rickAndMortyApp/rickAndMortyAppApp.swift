a216faa9e5839cfe61d8d84b01c8af7c0a2fae5d
Cristian Gutierrez <instanciageek@gmail.com>
2023-12-16 18:28:10 -0300

    Initial Commit

---
+++ rickAndMortyApp/rickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickAndMortyAppApp.swift
+//  rickAndMortyApp
+//
+//  Created by Cristian Gutiérrez on 16-12-23.
+//
+
+import SwiftUI
+
+@main
+struct rickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
